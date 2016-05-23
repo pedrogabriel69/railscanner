5742dfaa32684167fa4e8486f48c00bf14eb1bef
gabriel <pedrogabriel@mail.ru>
2016-05-23 15:49:38 +0300

    correct models

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,9 +1,9 @@
 class RailwayStation < ActiveRecord::Base
-  validates :title presence: true
-
   has_many :trains
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
   has_many :initial_point, class_name: 'Ticket', foreign_key: :initial_station_id
   has_many :final_point, class_name: 'Ticket', foreign_key: :final_station_id
+  
+  validates :title presence: true
 end
