2a6c34ed2c1de10d431bc00bd246262cb2e782b0
gabriel <pedrogabriel@mail.ru>
2016-06-07 16:24:46 +0300

    Interface change position station in route

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -4,6 +4,23 @@ class RailwayStation < ActiveRecord::Base
   has_many :routes, through: :railway_stations_routes
   has_many :initial_point, class_name: 'Ticket', foreign_key: :initial_station_id
   has_many :final_point, class_name: 'Ticket', foreign_key: :final_station_id
+
+  scope :ordered, -> { joins(:railway_stations_routes).order("railway_stations_routes.position").uniq }
   
   validates :title, presence: true
+
+  def update_position(route, position)
+    station_route = station_route(route)
+    station_route.update(position: position) if station_route
+  end
+
+  def position_in(route)
+    station_route(route).try(:position)
+  end
+
+  protected
+
+  def station_route(route)
+    station_route ||= railway_stations_routes.where(route: route).first
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604165218) do
+ActiveRecord::Schema.define(version: 20160607122734) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160604165218) do
     t.integer "railway_station_id"
     t.integer "route_id"
     t.integer "number"
+    t.integer "position"
   end
 
   create_table "routes", force: :cascade do |t|
