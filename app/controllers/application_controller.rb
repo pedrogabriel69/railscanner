d2c96eaee4149583cefbff26d3f85f5395773b32
pedrogabriel69 <pedrogabriel.gp69@gmail.com>
2016-06-23 22:22:04 +0300

    Bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(current_user)
-    current_user.admin? ? admin_admin_index_path : root_path
-  end
+  # def after_sign_in_path_for(current_user)
+  #  current_user.admin? ? admin_admin_index_path : root_path
+  # end
 
   protected
 
