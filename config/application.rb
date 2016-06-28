4d1cd9594b72d3954d951971ce109ecf05421ed8
pedrogabriel69 <pedrogabriel.gp69@gmail.com>
2016-06-28 12:35:15 +0300

    convert to pdf with pdfkit

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'pdfkit'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +23,7 @@ module Railscanner
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_controller.asset_host = 'railscanner.herokuapp.com'
+    config.middleware.use "PDFKit::Middleware", :print_media_type => true
   end
 end
